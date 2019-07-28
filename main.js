4eba059972cba3fc6d887b76b76692e4f94e5b2e
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-28 11:57:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    console.log("main.js: success");
     var index = window.location.pathname.split('/')[1];
     if (!index) {
         $('li.index').addClass('active');
