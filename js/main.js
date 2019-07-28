ec88dac397f69d317e3fdd813d6e57ec12d20b93
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-28 12:22:29 -0700

    Update jQuery

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     console.log("main.js: success");
+    
+    $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
+    $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
+
     var index = window.location.pathname.split('/')[1];
     if (!index) {
         $('li.index').addClass('active');
