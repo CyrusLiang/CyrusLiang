a632cfa73a78bf1a908ddf54879d297894572df4
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-29 12:26:13 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 $(function() {
-    console.log('hello!');
+    console.log('document');
     $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
     $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
-
+});
+$(window).on('load', function() {
+    console.log('window');
     var path = window.location.pathname.split('/').pop();
     if (path == '') path = './';
     $('nav a[href="' + path + '"]').addClass('current');
