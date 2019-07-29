dc334a37669191f98cfbd50a84c66e68adbf1fc2
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-28 19:24:37 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
     $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
     var index = window.location.pathname.split('/')[1];
     try {
+        console.log('try');
         $('li.' + index).addClass('active');
     }
     catch(err) {
+        console.log('err');
         $('li.index').addClass('active');
     }
 
