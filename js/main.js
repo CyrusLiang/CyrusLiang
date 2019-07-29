1c2242e96d4f6d5f19ae65399a0475882f12bdc6
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-29 11:59:16 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,10 @@
-function loadStuff() {
-    console.log('loadStuff');
-    $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');0
-    $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
-}
-
-function doStuff() {
-    console.log('doStuff');
-    var path = window.location.pathname.split("/").pop();
-    if (path == '') {
-        path = './';
-    }
-    var target = $('nav a[href="' + path + '"]');
-
-    target.addClass('current');
-}
-
-$(document).ready(function() {
+$(document).ready(function($) {
     console.log("hello!");
-    loadStuff();
-    doStuff();
+    $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
+    $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
+
+    var path = window.location.pathname.split("/").pop();
+    if (path == '') path = './';
+    var target = $('nav a[href="' + path + '"]');
+    target.addClass('current');
 });
