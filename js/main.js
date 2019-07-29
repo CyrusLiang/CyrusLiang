54ea8537d7c13a6a1185c2c44abc0232bd541768
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-29 12:47:41 -0700

    Update jQuery

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-$(function() {
+$(function () {
     console.log('hello!');
     $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
     $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
 
-    var url = window.location.href;
-    // url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
-    // url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
-    url = url.substr(url.lastIndexOf("/") + 1);
-    
-    if (url == '') url = './';
-    $('nav li').each(function() {
-        var href = $(this).find('a').attr('href');
-        if(url == href) $(this).addClass('active');
+    $('nav a').each(function () {
+        console.log(window.location.pathname);
+        if ($(this).attr("href") == window.location.pathname) {
+            $(this).attr("id", "active");
+        }
     });
 });
