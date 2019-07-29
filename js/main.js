6e0701e37f803a7ac77d4f1a194e8980c0b30cd4
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-29 12:26:32 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-    console.log('document');
+    console.log('hello!');
     $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
     $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
 });
