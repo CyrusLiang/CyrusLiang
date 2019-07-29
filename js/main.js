824cc77bd54e58cc5a83a53444de10d6dcde2597
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-28 19:31:38 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
-    console.log("main.js: success");
+    console.log("hello!");
     
     $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
     var index = window.location.pathname.split('/')[1];
-    $('li.' + index).addClass('active');
+    (!index) ? $('nav .index').addClass('active') : $('nav .' + index).addClass('active');
 
     $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
 });
