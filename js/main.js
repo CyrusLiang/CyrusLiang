65fff25cf77cd9a601bceca43da77e628090d26b
Cyrus Liang <thecyrusliang@gmail.com>
2019-07-28 18:34:39 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@ $(document).ready(function() {
     console.log("main.js: success");
     
     $('#header').load('https://www.cyrusliang.com/global/header.html #header-bar');
-    $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
-
     var index = window.location.pathname.split('/')[1];
     if (!index) {
         $('li.index').addClass('active');
     } else {
         $('li.' + index).addClass('active');
     }
+    
+    $('#footer').load('https://www.cyrusliang.com/global/footer.html #footer-bar');
 });
